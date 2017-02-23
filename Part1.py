53175ac972b217f5ca19e1ea7247c395bbafaa41
Jules Bstock <jules.bstock@hotmail.fr>
2017-02-23 19:08:15 +0100

    first version code. oop attempt, probably easier to switch to regular method.

--- Part1.py
+++ Part1.py
@@ -1,18 +1,21 @@
 class Stbond(object):
-    def __init__(self, term, amount, price):
+    def __init__(self, term, amount, interest):
         self.term = term
         self.amount = amount
-        self.price = price
+        self.interest = interest
 
-        if price <= 1000:
-            print("Minimum price to invest is 1000!")
+        if amount <= 1000:
+            print("Minimum price to invest is 1000")
         if term <= 2:
             print("Minimum term for the sort term bond is 2 years")
 
+    def stbond_return(self, amount, interest, term):
+        self.amount = amount
+        self.term = term
+        self.interst = interest
+        return amount * 0.01 * 100
 
-    '''def stbond_return(self):
-        self.price * self.term = x
-        return x '''
+print(Stbond.stbond_return(1000))
 
 
 
