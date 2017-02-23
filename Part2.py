2e44e2847f7780a0466e2e22f34d00333d906a40
Jules Bstock <jules.bstock@hotmail.fr>
2017-02-23 21:06:27 +0100

    starting part 2

--- Part2.py
+++ Part2.py
@@ -9,10 +9,11 @@ allfiles = glob.glob(os.path.join(path,"*.csv"))
 
 np_array_list = []
 for file in allfiles:
-    df = pd.read_csv(file, index_col=None)
-    np_array_list.append(df.as_matrix())
+    df = pd.read_csv(file, sep=';', index_col=0, parse_dates=True)
+    #np_array_list.append(df.as_matrix())
+    print(df)
+#comb_np_array = np.vstack(np_array_list)
+#big_frame = pd.DataFrame(comb_np_array)
 
-comb_np_array = np.vstack(np_array_list)
-big_frame = pd.DataFrame(comb_np_array)
+#print(big_frame)
 
-print(big_frame)
