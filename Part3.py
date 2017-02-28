f9f141c1212d4058fd20c4fb7e2d35bc19fe0928
Jules Bstock <jules.bstock@hotmail.fr>
2017-02-28 22:16:06 +0100

    Trying part 3 update

--- Part3.py
+++ Part3.py
@@ -13,18 +13,24 @@ class Investor(object):
         if self.investment_mode == "defensive":
             while self.budget > 0:
                 select = random.choice([1000, 3000])
-                self.budjet = int(self.budjet - int(select))
+                self.budget = int(self.budget - int(select))
             return portfolio
         if self.investment_mode == "aggressive":
             while self.budget > 0:
                 select = random.choice(["AAPL", "GOOGL", "YHOO", "AXP", "XOM", "KO", "NOK", "MS", "IBM", "FDX"])
-                self.budjet = int(select*random.choice(0, 0.5*max(self.budget/int(select['High']))))
-            if self.budget < 100:
-                break
+                self.budget = int(self.budget - int(int(select)*random.choice(0, 0.5 * max(self.budget/int(select['High'])))))
+                if self.budget < 100:
+                    break
             return portfolio
         if self.investment_mode == "mixed":
             while self.budget > 0:
                 select = random.choice(random.choice([1000, 3000]),["AAPL", "GOOGL", "YHOO", "AXP", "XOM", "KO", "NOK", "MS", "IBM", "FDX"])
-            return portfolio
-        else:
-             break
+                If random.choice(random.choice([1000, 3000]) == True:
+                    select = random.choice([1000, 3000])
+                    self.budget = int(self.budget - int(select))
+                Elif
+                    select = random.choice(["AAPL", "GOOGL", "YHOO", "AXP", "XOM", "KO", "NOK", "MS", "IBM", "FDX"])
+                    self.budget = int(self.budget - int(int(select) * random.choice(0, 0.5 * max(self.budget / int(select['High'])))))
+                return portfolio
+            else:
+                break
