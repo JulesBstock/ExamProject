4b26a15ab75483e2a17195bd9cf4c8df9f92dce9
Jules Bstock <jules.bstock@hotmail.fr>
2017-02-28 09:54:54 +0100

    Part 1 and 2 finished

--- Part2.py
+++ Part2.py
@@ -1,103 +1,82 @@
-import os
-import glob
-import pandas as pd
-import numpy as np
-import webbrowser
 import datetime
 import matplotlib.pyplot as plt
-import random
 import pandas_datareader.data as web
 
-path = "C:/Users/jules/Documents/M2/Python/ExamProject/Data"
-allfiles = glob.glob(os.path.join(path,"*.csv"))
-
-
-np_array_list = []
-for file in allfiles:
-    df = pd.read_csv(file, sep=';', index_col=0, parse_dates=True)
-    #np_array_list.append(df.as_matrix())
-    #print(df)
-#comb_np_array = np.vstack(np_array_list)
-#big_frame = pd.DataFrame(comb_np_array)
-
-#print(big_frame)
-#Yahoo = df.ix[:,0]
-#print(Yahoo)
-
-start = datetime.date(2005, 1, 1)
+beg = datetime.date(2005, 1, 1)
 end = datetime.date(2015, 12, 31)
-AAPL=web.DataReader("AAPL", 'yahoo', start, end)
-GOOGL=web.DataReader("GOOGL", 'yahoo', start, end)
-YHOO=web.DataReader("YHOO", 'yahoo', start, end)
-AXP= web.DataReader("AXP", 'yahoo', start, end)
-XOM =web.DataReader("XOM", 'yahoo', start, end)
-KO= web.DataReader("KO", 'yahoo', start, end)
-NOK= web.DataReader("NOK", 'yahoo', start, end)
-MS=web.DataReader("MS", 'yahoo', start, end)
-IBM= web.DataReader("IBM", 'yahoo', start, end)
-FDX=web.DataReader("FDX", 'yahoo', start, end)
-AAPL['Date'] =AAPL.index.map(lambda x: str(x)[:10])
-GOOGL['Date'] =GOOGL.index.map(lambda x: str(x)[:10])
-YHOO['Date'] =YHOO.index.map(lambda x: str(x)[:10])
-AXP['Date'] =AXP.index.map(lambda x: str(x)[:10])
-XOM['Date'] =XOM.index.map(lambda x: str(x)[:10])
-KO['Date'] =KO.index.map(lambda x: str(x)[:10])
-NOK['Date'] =NOK.index.map(lambda x: str(x)[:10])
-MS['Date'] =MS.index.map(lambda x: str(x)[:10])
-IBM['Date'] =IBM.index.map(lambda x: str(x)[:10])
-FDX['Date'] =FDX.index.map(lambda x: str(x)[:10])
+
+AAPL = web.DataReader("AAPL", 'yahoo', beg, end)
+AXP = web.DataReader("AXP", 'yahoo', beg, end)
+FDX = web.DataReader("FDX", 'yahoo', beg, end)
+GOOGL = web.DataReader("GOOGL", 'yahoo', beg, end)
+IBM = web.DataReader("IBM", 'yahoo', beg, end)
+KO = web.DataReader("KO", 'yahoo', beg, end)
+MS = web.DataReader("MS", 'yahoo', beg, end)
+NOK = web.DataReader("NOK", 'yahoo', beg, end)
+XOM = web.DataReader("XOM", 'yahoo', beg, end)
+YHOO = web.DataReader("YHOO", 'yahoo', beg, end)
+
+AAPL['Date'] = AAPL.index.map(lambda x: str(x)[:10])
+AXP['Date'] = AXP.index.map(lambda x: str(x)[:10])
+FDX['Date'] = FDX.index.map(lambda x: str(x)[:10])
+GOOGL['Date'] = GOOGL.index.map(lambda x: str(x)[:10])
+IBM['Date'] = IBM.index.map(lambda x: str(x)[:10])
+KO['Date'] = KO.index.map(lambda x: str(x)[:10])
+MS['Date'] = MS.index.map(lambda x: str(x)[:10])
+NOK['Date'] = NOK.index.map(lambda x: str(x)[:10])
+XOM['Date'] = XOM.index.map(lambda x: str(x)[:10])
+YHOO['Date'] = YHOO.index.map(lambda x: str(x)[:10])
 
 class Stock(object):
-    def __init__(self,term,amount,stockname,date):
-        self.term=term
-        self.amount=amount
-        self.date=date
-        if stockname=="AAPL":
-            self.stockname = "AAPL"
-            self.df= AAPL
-        elif stockname=="GOOGL":
-            self.stockname="GOOGL"
-            self.df = GOOGL
-        elif stockname == "YHOO":
-            self.stockname = "YHOO"
-            self.df = YHOO
-        elif stockname == "AXP":
-            self.stockname = "AXP"
+    def __init__(self, term, amount, stock, date):
+        self.term = term
+        self.amount = amount
+        self.date = date
+        if stock == "AAPL":
+            self.stock = "AAPL"
+            self.df = AAPL
+        elif stock == "AXP":
+            self.stock = "AXP"
             self.df = AXP
-        elif stockname == "XOM":
-            self.stockname = "XOM"
-            self.df = XOM
-        elif stockname == "KO":
-            self.stockname = "KO"
-            self.df = KO
-        elif stockname == "NOK":
-            self.stockname = "NOK"
-            self.df = NOK
-        elif stockname == "MS":
-            self.stockname = "MS"
-            self.df = MS
-        elif stockname == "IBM":
-            self.stockname = "IBM"
-            self.df = IBM
-        elif stockname == "FDX":
-            self.stockname = "FDX"
+        elif stock == "FDX":
+            self.stock = "FDX"
             self.df = FDX
+        elif stock == "GOOGL":
+            self.stock = "GOOGL"
+            self.df = GOOGL
+        elif stock == "IBM":
+            self.stock = "IBM"
+            self.df = IBM
+        elif stock == "KO":
+            self.stock = "KO"
+            self.df = KO
+        elif stock == "MS":
+            self.stock = "MS"
+            self.df = MS
+        elif stock == "NOK":
+            self.stock = "NOK"
+            self.df = NOK
+        elif stock == "XOM":
+            self.stock = "XOM"
+            self.df = XOM
+        elif stock == "YHOO":
+            self.stock = "YHOO"
+            self.df = YHOO
         else:
-            self.stockname=False
-            self.df=False
+            self.stock = False
+            self.df = False
 
 
-a=AAPL['High'].plot(label='Apple')
-b=GOOGL['High'].plot(label='Google')
-c=YHOO['High'].plot(label='Yahoo')
-d=AXP['High'].plot(label='American Express Company')
-e=XOM['High'].plot(label='Exxon')
-f=KO['High'].plot(label='Coca-Cola')
-g=NOK['High'].plot(label='Nokia')
-h=MS['High'].plot(label='Microsoft')
-i=IBM['High'].plot(label='IBM')
-j=FDX['High'].plot(label='Fedex')
+x1 = AAPL['High'].plot(label='AAPL')
+x2 = AXP['High'].plot(label='AXP')
+x3 = FDX['High'].plot(label='FDX')
+x4 = GOOGL['High'].plot(label='GOOGL')
+x5 = IBM['High'].plot(label='IBM')
+x6 = KO['High'].plot(label='KO')
+x7 = MS['High'].plot(label='MS')
+x8 = NOK['High'].plot(label='NOK')
+x9 = XOM['High'].plot(label='XOM')
+x10 = YHOO['High'].plot(label='YHOO')
 
-# Put a legend to the right of the current axis
-a.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+x1.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 plt.show()
